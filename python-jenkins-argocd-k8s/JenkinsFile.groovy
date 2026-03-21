726ab7723fc5fef6694e03e0b8f2f209b60a160b
Neha409 <nehagupta22sep@gmail.com>
2026-03-22 01:14:56 +0530

    update

--- python-jenkins-argocd-k8s/JenkinsFile.groovy
+++ python-jenkins-argocd-k8s/JenkinsFile.groovy
@@ -1,3 +1,43 @@
 pipeline { 
 
+    agent {
+        docker {
+            image 'docker:24'
+            args '-v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
+
+    stages {
+        stage("GIT checkout") {
+        steps {
+            git branch: 'main',
+                    credentialsId: 'github-token',
+                    url: 'https://github.com/Neha409/Jenkins-Zero-To-Hero.git'
+         }
+        }
+
+        stage("build image"){
+            steps {
+                script {
+                    sh '''
+                        echo 'Buid Docker Image'
+                        docker build -t neha/cicd-e2e:${BUILD_NUMBER}
+
+                    '''
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
+
   
